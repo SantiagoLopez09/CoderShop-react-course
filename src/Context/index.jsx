a65296970bf75afb2c8c26293f3e9ed476c5abe9
Santiago Lopez <santiagolopeza30@gmail.com>
2023-06-13 12:09:13 -0500

    Detalle de producto e iconos TailWindCSS

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,16 +1,18 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
 
-export const ShoppingCartContext = createContext()
+export const ShoppingCartContext = createContext();
 
-export const ShppingCartProvider = ({children}) => {
-  const [count, setCount] = useState(0)
+export const ShppingCartProvider = ({ children }) => {
+  const [count, setCount] = useState(0);
 
-  return(
-    <ShoppingCartContext.Provider value={{
-      count,
-      setCount
-    }}>
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        count,
+        setCount,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
-  )
-}
+  );
+};
