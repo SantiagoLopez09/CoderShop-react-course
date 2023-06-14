f1224083565d34ca1091b0a335402854722403ee
Santiago Lopez <santiagolopeza30@gmail.com>
2023-06-14 16:38:08 -0500

    Realizando Reto3

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,102 +1,146 @@
-import { useContext } from "react";
+import { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
 import { ShoppingBagIcon } from '@heroicons/react/24/solid'
-import { NavLink } from "react-router-dom";
-import { ShoppingCartContext } from "../../Context";
+import { ShoppingCartContext } from '../../Context'
 
 const Navbar = () => {
-  const context = useContext(ShoppingCartContext);
-  const activeStyle = "underline underline-offset-4";
+  const context = useContext(ShoppingCartContext)
+  const activeStyle = 'underline underline-offset-4  '
+
+  // Sign Out
+  const signOut = localStorage.getItem('sign-out')
+  const parsedSignOut = JSON.parse(signOut)
+  const isUserSignOut = context.signOut || parsedSignOut
+
+  const handleSignOut = () => {
+    const stringifiedSignOut = JSON.stringify(true)
+    localStorage.setItem('sign-out', stringifiedSignOut)
+    context.setSignOut(true)
+  }
+
+  const renderView = () => {
+    if (isUserSignOut) {
+      return (
+        <li>
+          <NavLink
+            to="/sign-in"
+            className={({ isActive }) => isActive ? activeStyle : undefined }
+            onClick={() => handleSignOut()}>
+            Sign out
+          </NavLink>
+        </li>
+      )
+    } else {
+      return (
+        <>
+          <li className='text-black/60'>
+            teff@platzi.com
+          </li>
+          <li>
+            <NavLink
+              to='/my-orders'
+              className={({ isActive }) => isActive ? activeStyle : undefined}>
+              My Orders
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/my-account'
+              className={({ isActive }) => isActive ? activeStyle : undefined}>
+              My Account
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/sign-in'
+              className={({ isActive }) => isActive ? activeStyle : undefined}
+              onClick={() => handleSignOut()}>
+              Sign out
+            </NavLink>
+          </li>
+        </>
+      )
+    }
+  }
 
   return (
-    <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-white">
-      <ul className="flex items-center gap-3">
-        <li className="font-semibold text-lg">
-          <NavLink to="/">Shopi</NavLink>
+    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-white'>
+      <ul className='flex items-center gap-3'>
+        <li className='font-semibold text-lg'>
+          <NavLink to='/'>
+            Shopi
+          </NavLink>
         </li>
         <li>
           <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/'
+            onClick={() => context.setSearchByCategory()}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
             All
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/clothes"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/clothes'
+            onClick={() => context.setSearchByCategory('clothes')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
             Clothes
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/electronics"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/electronics'
+            onClick={() => context.setSearchByCategory('electronics')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
             Electronics
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/furnitures"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/furnitures'
+            onClick={() => context.setSearchByCategory('furnitures')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
             Furnitures
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/toys"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/toys'
+            onClick={() => context.setSearchByCategory('toys')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
             Toys
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/others"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/others'
+            onClick={() => context.setSearchByCategory('others')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
             Others
           </NavLink>
         </li>
       </ul>
-      <ul className="flex items-center gap-3">
-        <li className="text-black/60">santiagolopez@gmail.com</li>
-        <li>
-          <NavLink
-            to="/my-orders"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            My Orders
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/my-account"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            My Account
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/sign-in"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Sign In
-          </NavLink>
-        </li>
+      <ul className='flex items-center gap-3'>
+        {renderView()}
         <li className='flex items-center'>
           <ShoppingBagIcon className='h-6 w-6 text-black'></ShoppingBagIcon>
-          <div>
-            {context.count}
-          </div>
+          <div>{context.cartProducts.length}</div>
         </li>
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -2,7 +2,34 @@ import { createContext, useState, useEffect} from "react";
 
 export const ShoppingCartContext = createContext();
 
+export const initializeLocalStorage = () => {
+  const accountInLocalStorage = localStorage.getItem('account')
+  const signoutInLocalStorage = localStorage.getItem('sign-out')
+  let parsedAccount
+  let parsedSignOut
+
+  if (!accountInLocalStorage){
+    localStorage.setItem('account', JSON.stringify([]))
+    parsedAccount={}
+  }else{
+    parsedAccount =JSON.parse(accountInLocalStorage)
+  }
+
+  if(!signoutInLocalStorage){
+    localStorage.setItem('sign-out', JSON.stringify(false))
+    parsedSignOut= false
+  }else{
+    parsedSignOut= JSON.parse(signoutInLocalStorage)
+  }
+}
+
 export const ShoppingCartProvider = ({ children }) => {
+  
+  // My Account
+  const [account, setAccount] = useState([])
+  
+  // Signo Out
+  const [signOut, setSignOut] = useState(false)
   
   // Shopping Cart - Increment Quantity
   const [count, setCount] = useState(0);
@@ -33,15 +60,52 @@ export const ShoppingCartProvider = ({ children }) => {
 
 // Get products
 const [items, setItems] = useState(null)
+const [filteredItems, setFilteredItems] = useState(null)
 
 // Get products by titles
 const [searchByTitle, setSearchByTitle] = useState(null)
+
+// Get products by Category
+const [searchByCategory, setSearchByCategory] = useState(null)
 
   useEffect(() => {
     fetch ('https://api.escuelajs.co/api/v1/products')
       .then(response => response.json())
       .then(data =>  setItems(data))
   }, []) 
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+  
+  const filteredItemsByCategory = (items, searchByCategory) => {
+    return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+  }
+
+  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === 'BY_TITLE') {
+      return filteredItemsByTitle(items, searchByTitle)
+    }
+
+    if (searchType === 'BY_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory)
+    }
+
+    if (searchType === 'BY_TITLE_AND_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    if (!searchType) {
+      return items
+    }
+  }
+
+  useEffect(() => {
+    if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
+    if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && !searchByCategory) setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
+  }, [items, searchByTitle, searchByCategory])
 
 
   return (
@@ -65,6 +129,13 @@ const [searchByTitle, setSearchByTitle] = useState(null)
         setItems,
         searchByTitle,
         setSearchByTitle,
+        filteredItems,
+        searchByCategory,
+        setSearchByCategory,
+        account,
+        setAccount,
+        signOut,
+        setSignOut,
       }}
     >
       {children}
